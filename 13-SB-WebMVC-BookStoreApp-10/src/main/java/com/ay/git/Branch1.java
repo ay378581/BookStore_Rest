7ccb0a3a255b1193d7e6e06f9728656daa788079
Dev1 <Dev1@gmail.com>
2024-06-13 13:13:21 +0530

    batch1() Branch.java - 2:30

--- 13-SB-WebMVC-BookStoreApp-10/src/main/java/com/ay/git/Branch1.java
+++ 13-SB-WebMVC-BookStoreApp-10/src/main/java/com/ay/git/Branch1.java
@@ -9,5 +9,12 @@ public class Branch1 {
 	public void y() {
 		System.out.println("Branch1.y()");
 	}
-
+	
+	public void z() {
+		System.out.println("Branch1.z()");
+	}
+	
+	public void batch1pm() {
+		System.out.println("Branch1.batch1pm()");
+	}
 }
